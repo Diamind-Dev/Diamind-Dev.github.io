b18194c242c4d04dedc2bca257be6e7fff161cd5
Diamind-Dev <diamind.group@gmail.com>
2024-10-14 14:28:13 +0330

    Add files via upload

--- Slider.js
+++ Slider.js
@@ -1,3 +1,25 @@
+function setBr(){
+  let demoUEName = document.getElementById("UEProjectName");
+  let demoAppName = document.getElementById("demoAppName");
+  let isMobVewLes428= window.matchMedia("(max-width:428px)").matches;
+  let isMobVewLes340 = window.matchMedia("(max-width:340px)").matches;
+  console.log("isMobileViewLessThan340px:", isMobVewLes340); // Check if the media query
+  console.log("isMobileViewLessThan428px:", isMobVewLes428); // Check if the media query
+  if(isMobVewLes428 && isMobVewLes340){
+    demoUEName.innerHTML = "ANFP_Demo_UE<br>_ModereatorFiles",
+    demoAppName.innerHTML = "ANFP_Demo_APK<br>_v1.0";
+  }else if(isMobVewLes428){
+    demoUEName.innerHTML = "ANFP_Demo_UE<br>_ModereatorFiles",
+    demoAppName.innerHTML = "ANFP_Demo_APK_v1.0";
+  }else 
+    demoUEName.innerHTML = "ANFP_Demo_UE_ModereatorFiles",
+    demoAppName.innerHTML = "ANFP_Demo_APK_v1.0";
+}
+
+setBr();
+window.addEventListener("resize" , setBr);
+
+
 let slideIndex = 1;
 showSlides(slideIndex);
 
@@ -44,3 +66,16 @@ function showSlides() {
   slides[slideIndex-1].style.display = "block";
   setTimeout(showSlides, 6000); // Change image every 2 seconds
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+function closeMenuEvent(){
+  let slideMenu = document.getElementsByClassName("menuPaneDiv")[0];
+  slideMenu.style.transform='translateX(0vw)';
+}
+
+let linkButtons = document.getElementsByClassName("DDMItemA");
+for(let i=0; i<linkButtons.length; i++){
+linkButtons[i].addEventListener("click",closeMenuEvent);
+}
+});
+
